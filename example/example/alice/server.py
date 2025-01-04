5af613a34ec91b55ab849452021ecb272bead6d5
Oleksandr Draha <odraha@bcdtriptech.com>
2025-01-04 22:30:13 +0200

    [MMM-1] Create simple setup

---
+++ example/example/alice/server.py
@@ -0,0 +1,25 @@
+import requests
+from flask import Flask
+
+from example.utils import http
+
+
+class AliceServer(Flask):
+
+    def __init__(self, alice_host, alice_port, bob_host, bob_port):
+        super(AliceServer, self).__init__(__name__)
+
+        self.alice_host = alice_host
+        self.alice_port = alice_port
+        self.bob_host = bob_host
+        self.bob_port = bob_port
+
+        self.add_url_rule("/hello", view_func=self.hello_request, methods=["POST"])
+
+    def run_server(self):
+        self.run(host=self.alice_host, port=self.alice_port)
+
+    def hello_request(self):
+        url = http(f"{self.bob_host}:{self.bob_port}/hello")
+        bob_response = requests.post(url=url)
+        return bob_response.text
