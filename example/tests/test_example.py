81febc6506f11400fb4bb64ff6a694e39efa25dd
Oleksandr Draha <13730918+oleksandr-draha@users.noreply.github.com>
2025-01-09 20:45:13 +0200

    [MMM-21] Move custom processors to custom methods (#22)

--- example/tests/test_example.py
+++ example/tests/test_example.py
@@ -9,6 +9,6 @@ ALICE_PORT = os.getenv("ALICE_PORT", "50517")
 
 @pytest.mark.force_proxy
 def test_hello(mocks_group):
-    with mocks_group('hello'):
+    with mocks_group("hello"):
         response = Session().post(url=f"http://{ALICE_HOST}:{ALICE_PORT}/hello")
-        assert response.text == 'world'
+        assert response.text == "world"
